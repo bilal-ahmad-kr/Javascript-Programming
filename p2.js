e8ae7b828145ad9066545b1cb4230c430d89d49c
bilal98739 <bilalhassan779966@gmail.com>
2026-09-16 14:48:49 +0500

    p2.js is added

---
+++ p2.js
@@ -0,0 +1,12 @@
+function checkNumber(num){
+    if( num > 0){
+        console.log("Number is positive")
+    } else if(num < 0){
+        console.log("Number is negative")
+    } else {
+        console.log("Number is zero")
+    }
+}
+checkNumber(10)
+checkNumber(-5)
+checkNumber(0)
